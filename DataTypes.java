643f6957f8655e65228f69a62782181f2e891a5f
lemcode1 <114827935+lemcode1@users.noreply.github.com>
2024-01-15 21:20:11 +0530

    HelloWorld, variables

---
+++ DataTypes.java
@@ -0,0 +1,23 @@
+public class DataTypes {
+    public static void main(String[] args){
+        int a=10;  // declaring and initialing the data
+        int b;     // declaring b variable
+        b = 20;    // initializing b value
+        System.out.print("print a value ");
+        System.out.print(" "+a);
+        System.out.println(" "+b);
+        System.out.println(" "+b);
+        System.out.print("\""+a+"+"+b+"="+(a+b)+"\"");
+
+        String name = "kasi";
+        //10+20=30
+
+    }
+  // Sir can we print output as “10+20=30”
+}
+//20
+//10
+//
+//10+20=30
+//20-10=10
+//20*10=200
---
+++ Variables.java
@@ -0,0 +1,15 @@
+public class Variables {
+    public static void main(String[] args) {
+        int _a =10;
+        int _A = 30;
+        int _$ = 20;
+        int abc123= 5678;
+//        int 123abc= 5678;
+        System.out.println(abc123);
+        System.out.println(_A);
+        System.out.println(_$);
+//        int class =30;
+        System.out.print("");
+    }
+
+}
